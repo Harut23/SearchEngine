85d200f3e28bb34097648fe9881dcccfb353326b
harut23 <harutmargaryan.am@gmail.com>
2022-02-12 12:38:21 +0400

    FindLinks function has been added

--- src/com/company/Crawler.java
+++ src/com/company/Crawler.java
@@ -6,9 +6,11 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Set;
 
-public class Crawler {
+public class Crawler extends Parser {
 
 private static final int Max_Depth = 2;
 
@@ -18,29 +20,65 @@ public Crawler(){
     links = new HashSet<>();
 }
 
-public void getPageLinks(String URL,int Depth){                  //URL i meji sax linkery talis a
-    if((!links.contains(URL) && (Depth < Max_Depth))){
-        System.out.println(">>Depth:: " + Depth +"["+ URL +"]");
+    //we get all URLs from a web page
+    public static Set<String> findLinks(String url) throws IOException {
 
-        try {
-            links.add(URL);
+        Set<String> links = new HashSet<>();
 
-            Document document = Jsoup.connect(URL).get();
-            Elements linksOnPage = document.select("a[href]");
+        Document doc = Jsoup.connect(url)
+               // .data("query", "Java")
+                //.userAgent("Mozilla")
+               // .cookie("auth", "token")
+                //.timeout(3000)
+                .get();
 
-            Depth++;
-            for (Element page : linksOnPage) {
-                getPageLinks(page.attr("abs:href"), Depth);
-            }
-        } catch (IOException e) {
-            System.err.println("For '" + URL + "': " + e.getMessage());
+        Elements elements = doc.select("a[href]");
+        for (Element element : elements) {
+            links.add(element.attr("href"));
         }
 
+        return links;
 
     }
+
+
+
+
+
+
+
+
+
+
+
+/*public void getPageLinks(String URL, int Depth) {                  //URL i meji sax linkery talis a
+//    HashSet<String> LinksOfUrl=new HashSet<>() ;
+
+       if ((!links.contains(URL) && (Depth < Max_Depth))) {
+           System.out.println(">>Depth:: " + Depth +"["+ URL +"]");
+
+
+           try {
+               links.add(URL);
+
+               Document document = Jsoup.connect(URL).get();
+               Elements linksOnPage = document.select("a[href]");
+
+               Depth++;
+               for (Element page : linksOnPage) {
+                   getPageLinks(page.attr("abs:href"), Depth);
+               }
+           } catch (IOException e) {
+               System.err.println("For '" + URL + "': " + e.getMessage());
+           }
+
+
+       }
+
+
 }
 
-
+*/
 
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,16 +5,25 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-       // new Crawler().getPageLinks("https://rau.am/",0);
+       // new  Crawler().getPageLinks("https://tun.am/am/",0);
+        Set<String>set = new Crawler().findLinks("https://rau.am/");
 
-      //  new Parser().parse("https://rau.am/");
+        for(String el: set){
+            System.out.println(el);
+        }
 
-new Parser().getTextFromHtml("https://rau.am/");
 
+
+     //  System.out.println(new Parser().parse("https://tun.am/am/"));
+
+      //new Parser().getTextFromHtml("https://tun.am/am/");
 
 
     }
